01a4830f1818e4b1b2a74d1fa972ef8c4f63fdaa
unknown <wltjdwls1823@naver.com>
2020-01-21 16:17:19 +0900

    half

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
-  return(
-    <div>
-      <h2> I like {name} </h2>
-     <img src={picture} />
-   </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
 
-const foodILIke = [
-  {
-    name: "Kimchi",
-    image:
-      "https://www.google.co.kr/url?sa=i&source=images&cd=&ved=2ahUKEwie85PvhYXnAhWUBIgKHdPcC-wQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.maangchi.com%2Frecipe%2Fchaesik-kimchi&psig=AOvVaw3GtcA36MP6Tpl0J9lcviS0&ust=1579158470397337"
-  },
-  {
-    name: "spongebob",
-    image:
-      "https://www.google.co.kr/url?sa=i&source=images&cd=&ved=2ahUKEwifrZOSiYXnAhXXMt4KHYZbCsEQjRx6BAgBEAQ&url=https%3A%2F%2Fprograms.sbs.co.kr%2Fnick%2Fspongebob%2Fcast%2F58220&psig=AOvVaw25DChgcQFqVAb4OLAWzmWO&ust=1579159344704053"
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 3000);
   }
-]
-
-function App() {
-  return (
-    <div>
-      {foodILIke.map(dish =>(
-        <Food name={dish.name}
-          picture={dish.image}
-         />
-        )
-       )
-      }
-    </div>
-  );
+  render(){
+    const{ isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
