1f732db6d7b4ace8f8a8eb51c788745c4e382851
unknown <wltjdwls1823@naver.com>
2020-01-22 15:48:33 +0900

    Updates

---
+++ precache-manifest.e040a60cf2f0d3b5f2b8feda3de8ad68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2110a2db441c7519c11fb971fcaaa7e",
+    "url": "/moive_app_2019/index.html"
+  },
+  {
+    "revision": "4284782761b4e46577fc",
+    "url": "/moive_app_2019/static/css/main.179c05b1.chunk.css"
+  },
+  {
+    "revision": "2285b3cdfe7d06f12a50",
+    "url": "/moive_app_2019/static/js/2.53a1a87c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/moive_app_2019/static/js/2.53a1a87c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4284782761b4e46577fc",
+    "url": "/moive_app_2019/static/js/main.beba3fd6.chunk.js"
+  },
+  {
+    "revision": "521b048ec78319ca290b",
+    "url": "/moive_app_2019/static/js/runtime-main.ddedbe27.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9203d5342db6f7419f3952d921a8b27e.js"
+  "/moive_app_2019/precache-manifest.e040a60cf2f0d3b5f2b8feda3de8ad68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moive_app_2019/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
